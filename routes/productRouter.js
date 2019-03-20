3057b702afe8b7ab90e5122538e85b3ba515ec9a
Tarek Islam <bayridge4th@gmail.com>
2019-03-20 03:38:10 -0400

    modularized db and started product routes (#5)

---
+++ routes/productRouter.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const productRouter = express.Router();
+
+const {productService} = require('../services/product')
+
+productRouter.get('/:prod_id',(req,res)=>{
+    const {prod_id} = req.params
+    productService.readProduct(prod_id)
+    .then(response=>{
+        res.json(response)
+    })
+    .catch(err=>{
+        res.json(err)
+    })
+})
+
+
+module.exports = productRouter
