db90ca4ab7dddf5eb65f65e85c7f82fef50ce32d
Tarek <bayridge4th@gmail.com>
2019-03-24 10:30:40 -0400

    edited helper test

--- tests/helper.test.js
+++ tests/helper.test.js
@@ -1,4 +1,4 @@
-const {createSqlCommandForUpdate} = require('../services/helper')
+const {createSqlCommandForUpdate2} = require('../services/helper')
 
 test("",()=>{
     expect('1').toEqual('1')
@@ -8,5 +8,5 @@ test("helper function test",()=>{
     const a = 'a'
     const b = 'b'
     //console.log(createSqlCommandForUpdate('7',[a,b],['a','b']))
-    expect(createSqlCommandForUpdate('7',[a,b],['a','b'],'users','id')).toBe('UPDATE users SET a=${a},b=${b} WHERE id=7')
+    expect(createSqlCommandForUpdate2([a,b],['a','b'],'users','id')).toBe('UPDATE users SET a=${a},b=${b} WHERE id=${id}')
 })
